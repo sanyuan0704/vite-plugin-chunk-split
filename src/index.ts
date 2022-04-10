2164f5db62a7e9327a77175ace5a1d540208ec07
sanyuan0704 <494130947@qq.com>
2022-04-10 15:50:38 +0800

    feat: support mpa

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,29 @@
-import { ManualChunksOption } from 'rollup';
-import { Plugin, resolvePackageEntry, resolvePackageData } from 'vite';
-import assert from 'assert';
-import path from 'path';
-import { ChunkSplit, CustomSplitting } from './types';
-import { staticImportedScan } from './staticImportScan';
-import { isCSSIdentifier } from './helper';
+import { ManualChunksOption } from "rollup";
+import { Plugin, resolvePackageEntry, resolvePackageData } from "vite";
+import assert from "assert";
+import path from "path";
+import { ChunkSplit, CustomSplitting } from "./types";
+import { staticImportedScan } from "./staticImportScan";
+import { isCSSIdentifier } from "./helper";
 
 const SPLIT_DEFAULT_MODULES: CustomSplitting = {
-  react_vendor: ['react', 'react-dom'],
-  lodash: ['lodash', 'lodash-es']
+  react_vendor: ["react", "react-dom"],
+  lodash: ["lodash", "lodash-es"],
 };
 
-const resolveEntry = (name: string): string => resolvePackageEntry(
-  name,
-  resolvePackageData(name, process.cwd(), true)!,
-  true,
-  {
-    isBuild: true,
-    isProduction: process.env.NODE_ENV === 'production',
-    isRequire: false,
-    root: process.cwd(),
-    preserveSymlinks: false,
-  },
-)!;
+const resolveEntry = (name: string): string =>
+  resolvePackageEntry(
+    name,
+    resolvePackageData(name, process.cwd(), true)!,
+    true,
+    {
+      isBuild: true,
+      isProduction: process.env.NODE_ENV === "production",
+      isRequire: false,
+      root: process.cwd(),
+      preserveSymlinks: false,
+    }
+  )!;
 
 const cache = new Map<string, boolean>();
 
@@ -30,27 +31,34 @@ const wrapCustomSplitConfig = (
   manualChunks: ManualChunksOption,
   customOptions: CustomSplitting
 ): ManualChunksOption => {
-  assert(typeof manualChunks === 'function');
+  assert(typeof manualChunks === "function");
   const groups = Object.keys(customOptions);
   // Create cache ahead of time to decrease the cost of resolve.sync!
   const depsInGroup: Record<string, string[]> = {};
   for (const group of groups) {
     const packageInfo = customOptions[group];
     depsInGroup[group] = packageInfo
-      .filter((item): boolean => typeof item === 'string')
+      .filter((item): boolean => typeof item === "string")
       .map((item) => {
         try {
           return resolveEntry(item as string);
         } catch (err) {
-          return '';
+          return "";
         }
       })
       .filter((_: string): boolean => _.length > 0);
   }
-  return (moduleId, { getModuleIds, getModuleInfo }): string | null | undefined => {
-    const isDepInclude = (id: string, depPaths: string[], importChain: string[]): boolean | undefined => {
-      const key = `${id}-${depPaths.join('|')}`;
-  
+  return (
+    moduleId,
+    { getModuleIds, getModuleInfo }
+  ): string | null | undefined => {
+    const isDepInclude = (
+      id: string,
+      depPaths: string[],
+      importChain: string[]
+    ): boolean | undefined => {
+      const key = `${id}-${depPaths.join("|")}`;
+
       // circular dependency
       if (importChain.includes(id)) {
         cache.set(key, false);
@@ -61,7 +69,9 @@ const wrapCustomSplitConfig = (
       }
       // hit
       if (depPaths.includes(id)) {
-        importChain.forEach(item => cache.set(`${item}-${depPaths.join('|')}`, true));
+        importChain.forEach((item) =>
+          cache.set(`${item}-${depPaths.join("|")}`, true)
+        );
         return true;
       }
       const moduleInfo = getModuleInfo(id);
@@ -69,8 +79,8 @@ const wrapCustomSplitConfig = (
         cache.set(key, false);
         return false;
       }
-      const isInclude = moduleInfo.importers.some(
-        importer => isDepInclude(importer, depPaths, importChain.concat(id))
+      const isInclude = moduleInfo.importers.some((importer) =>
+        isDepInclude(importer, depPaths, importChain.concat(id))
       );
       // set cache, important!
       cache.set(key, isInclude);
@@ -80,7 +90,11 @@ const wrapCustomSplitConfig = (
       const deps = depsInGroup[group];
       const packageInfo = customOptions[group];
       if (!isCSSIdentifier(moduleId)) {
-        if (moduleId.includes('node_modules') && deps.length && isDepInclude(moduleId, deps, [])) {
+        if (
+          moduleId.includes("node_modules") &&
+          deps.length &&
+          isDepInclude(moduleId, deps, [])
+        ) {
           return group;
         }
         for (const rule of packageInfo) {
@@ -93,34 +107,28 @@ const wrapCustomSplitConfig = (
     return manualChunks(moduleId, { getModuleIds, getModuleInfo });
   };
 };
-export function chunkSplitPlugin(splitOptions: ChunkSplit = {
-  strategy: 'default'
-}): Plugin {
-  const { strategy = 'default', customSplitting = {} } = splitOptions;
-  let manualChunks: ManualChunksOption; 
+export function chunkSplitPlugin(
+  splitOptions: ChunkSplit = {
+    strategy: "default",
+  }
+): Plugin {
+  const { strategy = "default", customSplitting = {} } = splitOptions;
+  let manualChunks: ManualChunksOption;
 
-  if (strategy === 'all-in-one') {
-    manualChunks = wrapCustomSplitConfig(
-      (id) => {
-        if (!isCSSIdentifier(id)) {
-          return 'index';
-        }
-      },
-      customSplitting
-    );
+  if (strategy === "all-in-one") {
+    manualChunks = wrapCustomSplitConfig(() => {
+      return null;
+    }, customSplitting);
   }
 
-  if (strategy === 'default') {
+  if (strategy === "default") {
     manualChunks = wrapCustomSplitConfig(
       (id, { getModuleInfo }): string | undefined => {
-        if (
-          id.includes('node_modules') &&
-          !isCSSIdentifier(id)
-        ) {
+        if (id.includes("node_modules") && !isCSSIdentifier(id)) {
           if (staticImportedScan(id, getModuleInfo, new Map(), [])) {
-            return 'vendor';
+            return "vendor";
           } else {
-            return 'async-vendor';
+            return "async-vendor";
           }
         }
       },
@@ -131,23 +139,20 @@ export function chunkSplitPlugin(splitOptions: ChunkSplit = {
     );
   }
 
-  if (strategy === 'unbundle') {
+  if (strategy === "unbundle") {
     manualChunks = wrapCustomSplitConfig(
       (id, { getModuleInfo }): string | undefined => {
-        if (
-          id.includes('node_modules') &&
-          !isCSSIdentifier(id)
-        ) {
+        if (id.includes("node_modules") && !isCSSIdentifier(id)) {
           if (staticImportedScan(id, getModuleInfo, new Map(), [])) {
-            return 'vendor';
+            return "vendor";
           } else {
-            return 'async-vendor';
+            return "async-vendor";
           }
         }
         const cwd = process.cwd();
-        if (!id.includes('node_modules') && !isCSSIdentifier(id)) {
+        if (!id.includes("node_modules") && !isCSSIdentifier(id)) {
           const extname = path.extname(id);
-          return path.relative(cwd, id).replace(extname, '');
+          return path.relative(cwd, id).replace(extname, "");
         }
         return;
       },
@@ -159,18 +164,17 @@ export function chunkSplitPlugin(splitOptions: ChunkSplit = {
   }
 
   return {
-    name: 'vite-plugin-chunk-split',
+    name: "vite-plugin-chunk-split",
     config() {
       return {
         build: {
           rollupOptions: {
             output: {
-              manualChunks
-            }
-          }
-        }
-      }
-    }
-  }
+              manualChunks,
+            },
+          },
+        },
+      };
+    },
+  };
 }
-
